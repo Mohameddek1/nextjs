6107bef02c040d34ed27d0aa4a2db068e93075c2
Mohameddek1 <mohadek2004@com>
2024-03-01 08:33:23 +0300

    first commit

---
+++ src/app/product/page.js
@@ -0,0 +1,11 @@
+const product = () => {
+    return ( 
+        <>
+        <h1>pruduct 1 </h1>
+        <h1>pruduct 2 </h1>
+        <h1>pruduct 3 </h1>
+        </>
+     );
+}
+ 
+export default product;
